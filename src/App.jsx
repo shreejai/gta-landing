d859f1ac69afde47f83ad59f63078f4c4818ff1f
Shreejai Raj <shreejai.raj@decidr.ai>
2026-03-26 20:30:02 +1100

    Update dependencies, add Tailwind CSS and GSAP support, and remove unused assets. Introduced new SVG images and fonts for improved design.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
       <div>
-        <h1>Welcome to the world of GTA and GSAP!</h1>
+        <h1 className='text-3xl text-pink'>Welcome to the world of GTA and GSAP!</h1>
       </div>
     </main>
   )
