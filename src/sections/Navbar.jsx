7e6683fe198c95787a92b826bafedc9f3a5c5f74
Shreejai Raj <shreejai.raj@decidr.ai>
2026-03-30 08:44:00 +1100

    Add Prettier configuration, integrate GSAP animations, and create new sections for the app

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="/images/nav-logo.svg" className="scale-90" alt="v logo" />
+      <img src="/images/menu.svg" className="w-10" alt="menu" />
+    </nav>
+  );
+};
+
+export default Navbar;
