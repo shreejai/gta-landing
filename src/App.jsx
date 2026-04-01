a937268495ee7a519cf85b2a5ea1a617cfebd35a
Shreejai Raj <shreejai.raj@decidr.ai>
2026-04-02 08:42:11 +1100

    Update index title and favicon add Outro section with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
 import Postcard from "./sections/Postcard";
 import Final from "./sections/Final";
+import Outro from "./sections/Outro";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -22,6 +23,7 @@ const App = () => {
       <Lucia />
       <Postcard />
       <Final />
+      <Outro />
     </main>
   );
 };
